178d781aa5d32a319bd72a7a79b476879787fd8a
MQuy <sugiacupit@gmail.com>
2019-05-19 00:09:17 +0200

    Support booting from fat32 hdd image

--- src/build.sh
+++ src/build.sh
@@ -9,10 +9,54 @@ else
   echo the file is not multiboot
 fi
 
-mkdir -p isodir/boot/grub
-cp mos.bin isodir/boot/mos.bin && rm mos.bin
-cp grub.cfg isodir/boot/grub/grub.cfg
-i386-elf-grub-mkrescue -o mos.iso isodir
+if [ "$2" == "iso" ]
+then
+  mkdir -p isodir/boot/grub
+  cp mos.bin isodir/boot/mos.bin && rm mos.bin
+  cp grub.cfg isodir/boot/grub/grub.cfg
+  i386-elf-grub-mkrescue -o mos.iso isodir
+else
+  # https://wiki.osdev.org/GRUB#HDD_Image_Instructions_for_OS_X_users
+  dd if=/dev/zero of=mos.img count=163840 bs=512
+  (
+  echo y
+  echo mos
+  echo n
+  echo edit 1
+  echo 0B
+  echo n
+  echo 2047
+  echo
+  echo write
+  echo quit
+  ) | fdisk -e mos.img
+  dd if=mos.img of=mbr.img bs=512 count=2047
+  dd if=mos.img of=fs.img bs=512 skip=2047
+  VOLUME_NAME=mos
+  DISK_NAME="$(hdiutil attach -nomount fs.img | cut -c6-10)"
+  newfs_msdos -F 32 $DISK_NAME
+  hdiutil detach $DISK_NAME
+  cat mbr.img fs.img > mos.img
+  hdiutil attach mos.img -mountpoint /Volumes/$VOLUME_NAME
+  /usr/local/sbin/i386-elf-grub-install --modules="part_msdos biosdisk fat multiboot configfile" --root-directory="/Volumes/${VOLUME_NAME}" mos.img
+  cp grub.cfg "/Volumes/${VOLUME_NAME}/boot/grub/grub.cfg"
+  cp mos.bin "/Volumes/${VOLUME_NAME}/boot/mos.bin"
+  hdiutil detach $DISK_NAME
+fi
 
-bochs -f .bochsrc -q -rc .debugrc
-# qemu-system-i386 -cdrom mos.iso -m 64M -d guest_errors,int
+if [ "$1" == "bochs" ]
+then
+  if [ "$2" == "iso" ]
+  then
+    bochs -f .bochsrc.cdrom -q -rc .debugrc
+  else
+    bochs -f .bochsrc.disk -q -rc .debugrc
+  fi
+else
+  if [ "$2" == "iso" ]
+  then
+    qemu-system-i386 -cdrom mos.iso -m 64M -d guest_errors,int
+  else
+    qemu-system-i386 -s -boot a -drive format=raw,file=./mos.img -d guest_errors,int
+  fi
+fi
